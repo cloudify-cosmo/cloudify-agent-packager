251efe0928e054ce66390bcdb9a5643ec06e3883
adamlavie <adaml@gigaspaces.com>
2014-12-24 17:14:48 +0200

    fixed broken package. removed problematic dependency

--- setup.py
+++ setup.py
@@ -54,7 +54,6 @@ setup(
     },
     install_requires=[
         "docopt==.0.6.1",
-        "infi.docopt_completion==0.2.1",
         "pyyaml==3.10",
         "virtualenv==1.11.4",
         "requests==2.4.1"
