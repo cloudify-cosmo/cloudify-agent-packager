d593d60b9d55e13feabc6f4f0c14d7c876504e5a
nir0s <nir36g@gmail.com>
2015-01-04 10:28:20 +0200

    oops, forgot codes file

---
+++ agent_packager/codes.py
@@ -0,0 +1,15 @@
+mapping = {
+    'dryrun_complete': 0,
+    'could_not_create_virtualenv': 1,
+    'virtualenv_already_exists': 2,
+    'could_not_download_file': 3,
+    'missing_cloudify_agent_config': 4,
+    'could_not_uninstall_module': 5,
+    'could_not_install_module': 6,
+    'failed_to_create_tar': 7,
+    'could_not_identify_distribution': 8,
+    'tar_already_exists': 9,
+    'installation_validation_failed': 10,
+    'could_not_access_config_file': 11,
+    'invalid_yaml_file': 12,
+}
