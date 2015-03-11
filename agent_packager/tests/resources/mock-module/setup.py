7c9a7db694ad70930399e0878876c711611ce5db
opencm <limor@gigaspaces.com>
2015-03-11 16:22:47 +0200

    Bump version to 3.2m7

--- agent_packager/tests/resources/mock-module/setup.py
+++ agent_packager/tests/resources/mock-module/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='mock_module',
-    version='3.2a6',
+    version='3.2a7',
     author='x',
     description='Data Feeder using different transports',
     packages=['mock_module'],
