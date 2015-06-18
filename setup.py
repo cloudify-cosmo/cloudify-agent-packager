14cf8466f52c8ceb9c0960d4820036034fc0701c
opencm <limor@gigaspaces.com>
2015-06-18 16:39:15 +0300

    Bump version to 3.3m2

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ class Tox(TestCommand):
 
 setup(
     name='cloudify-agent-packager',
-    version='3.3a1',
+    version='3.3a2',
     url='https://github.com/cloudify-cosmo/cloudify-agent-packager',
     author='Gigaspaces',
     author_email='cosmo-admin@gigaspaces.com',
